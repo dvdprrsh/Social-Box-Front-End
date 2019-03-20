64d21f5818c75f4de2b30327b310c0476ff13f5b
David <48495648+daveloper-dave@users.noreply.github.com>
2019-03-20 15:29:49 +0000

    Finished the main homepage layout and features. Moving onto the dashboard. Button on main homepage still does not do anything currently.
    Signed-off-by: David <48495648+Daveloper-Dave@users.noreply.github.com>

--- client_frontEnd/app/src/main/java/com/team36/client_frontend/MainActivity.java
+++ client_frontEnd/app/src/main/java/com/team36/client_frontend/MainActivity.java
@@ -5,26 +5,50 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private int[] friend_pps = {R.drawable.baseline_person_outline_black_36};
+    private String[] friend_names = {"Cam", "Josh", "Dave", "Cybs", "George", "Javier"};
+    private double[] friend_ratings = {4.2, 4.8, 4.0, 4.2, 4.8, 4.4};
 
-    private TextView mTextMessage;
+    private String[] journey_dates = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"};
+    private double[] journeyRatings = {2.8, 4.6, 4.4, 3.4, 4.8, 4.2};
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+    private BottomNavigationView.OnNavigationItemSelectedListener myBottomNavigationListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_allJourneys:
+                    return true;
+                case R.id.navigation_allFriends:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private BottomNavigationView.OnNavigationItemSelectedListener myAtAGlanceNavigationListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_friends:
+                    atAGlance_friends();
+                    return true;
+                case R.id.navigation_journeys:
+                    atAGlance_journeys();
                     return true;
             }
             return false;
@@ -36,14 +60,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(myBottomNavigationListener);
+
+        BottomNavigationView navigation_atAGlance = findViewById(R.id.navigation_atAGlance);
+        navigation_atAGlance.setOnNavigationItemSelectedListener(myAtAGlanceNavigationListener);
 
         myMain();
     }
 
-    private void myMain(){
+    private void atAGlance_friends(){
+        ArrayList<listview_item> allRows = new ArrayList<>();
+        ListView myListView = findViewById(R.id.listView_atAGlance);
 
+        for (int i=0; i<friend_names.length; i++){
+            listview_item oneRow = new listview_item();
+            oneRow.setImage_pp(friend_pps[0]);
+            oneRow.setText_name(friend_names[i]);
+            oneRow.setRating_stars((float)friend_ratings[i]);
+
+            allRows.add(oneRow);
+        }
+
+        myAdapter friends_adapter = new myAdapter(getApplicationContext(), allRows);
+        myListView.setAdapter(friends_adapter);
+    }
+
+    private void atAGlance_journeys(){
+        ArrayList<listview_itemJourneys> allRows = new ArrayList<>();
+        ListView myListView = findViewById(R.id.listView_atAGlance);
+
+        for (int i=0; i<journey_dates.length; i++){
+            listview_itemJourneys oneRow = new listview_itemJourneys();
+            oneRow.setText_journeyDate(journey_dates[i]);
+            oneRow.setRating_stars((float)journeyRatings[i]);
+
+            allRows.add(oneRow);
+        }
+
+        myAdapter_journeys journeys_adapter = new myAdapter_journeys(getApplicationContext(), allRows);
+        myListView.setAdapter(journeys_adapter);
+    }
+
+    private void myMain(){
+        atAGlance_friends();
     }
 }
